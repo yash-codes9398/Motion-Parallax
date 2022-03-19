be9e8d6d856fd238a388bb0e484d5ead15e0ff50
Yash Shah <yash.codes9398@gmail.com>
2022-03-20 02:52:11 +0530

    Add: Added code to support Motion Parallax effect

--- Motion Parallax/ViewController.swift
+++ Motion Parallax/ViewController.swift
@@ -6,14 +6,71 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    private let motionManager = CMMotionManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let backgroundImageView = UIImageView(frame: view.frame)
+        backgroundImageView.contentMode = .scaleAspectFill
+        backgroundImageView.image = UIImage(named: "bgImage")
+        view.addSubview(backgroundImageView)
+        
+        let overlayView = UIVisualEffectView(effect: UIBlurEffect(style: .systemUltraThinMaterialDark))
+        overlayView.frame = view.frame
+        view.addSubview(overlayView)
+        
+        let containerView = UIView()
+        containerView.clipsToBounds = true
+        containerView.layer.cornerRadius = 16
+        view.addSubview(containerView)
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        containerView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        containerView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40).isActive = true
+        containerView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        containerView.heightAnchor.constraint(equalToConstant: 450).isActive = true
+        
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.image = UIImage(named: "bgImage")
+//        imageView.clipsToBounds = true
+//        imageView.layer.cornerRadius = 16
+        containerView.addSubview(imageView)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        imageView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        
+        let imageViewCenter = view.center
+        
+        motionManager.startDeviceMotionUpdates(to: .main) { data, error in
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            }
+            guard let data = data else {
+                print("Error in the data")
+                return
+            }
+            let multiple: Double = 30
+            let xOffset = CGFloat(multiple * min(max(-1, data.attitude.roll), 1))
+            let yOffset = CGFloat(multiple * min(max(-1, data.attitude.pitch), 1))
+            print("X: \(data.attitude.roll) Y: \(data.attitude.pitch)")
+            UIView.animate(withDuration: 0.15) {
+//                imageView.center = CGPoint(x: imageViewCenter.x - xOffset, y: imageViewCenter.y - yOffset)
+                containerView.center = CGPoint(x: imageViewCenter.x + xOffset, y: imageViewCenter.y + yOffset)
+            }
+        }
     }
-
-
 }
 
